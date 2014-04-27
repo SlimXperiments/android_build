c5823eabba67b89c0e689132f29bc1bf3786f74b
Anthony King <anthonydking@gmail.com>
2014-04-27 21:46:17 +0100

    update automerger
    merge from slim as well now
    split the pushing away from the merging, closes the window more to any sync issues by others
    use the executable patch on repo to allow the script to use bash

--- tools/slim/auto-merge.sh
+++ tools/slim/auto-merge.sh
@@ -23,6 +23,11 @@
 #   login <username>
 #   password <password>
 
+# note that this script relies on a new feature to repo that is not yet merged
+# https://gerrit-review.googlesource.com/#/c/56419/
+
+export slim_loc="github"
+
 function check() {
   if [[ $? > 0 ]]; then
     lineno=$(expr ${BASH_LINENO} - 1)
@@ -32,56 +37,138 @@ function check() {
 }
 
 export AUTO_TOP=`pwd`
-repo forall '-ec' '
-  cd $AUTO_TOP
-  if [ $(grep "$REPO_PATH" .auto-merge_ignore) ]; then
-    echo "WARNING: ignoreing $REPO_PROJECT"
-    return 0
-  fi
+
+if [[ $* == *--slim* ]]; then
+if [[ $* != *--push* ]]; then
+repo forall '-x' '/bin/bash' '-ec' '
   if [ "$REPO_REMOTE" != "slimX" ]; then
-    return 0
+    exit 0
+  fi
+  cd $AUTO_TOP
+  blah=($(grep "$REPO_PATH" .auto-merge_ignore))
+  if [ -n "$blah" ]; then
+    bloo=${blah[1]}
+    if [ "$bloo" == "slim" ] || [ "$bloo" == "" ]; then
+      echo "WARNING: ignoreing $REPO_PROJECT"
+      exit 0
+    fi
+  fi
+  cd $REPO_PATH
+  echo "in $REPO_PATH"
+  git branch -a | grep "$slim_loc/kk4\.4" > /dev/null
+  if [ $? -gt 0 ]; then
+    echo "ERROR: $REPO_PROJECT: does not have $slim_loc/kk4.4"
+    exit 1
+  fi
+  git fetch $slim_loc
+  if [ $? -gt 0 ]; then
+    echo "ERROR: $REPO_PROJECT: failed to fetch $slim_loc/kk4.4"
+    exit 1
+  fi
+  git branch -a | grep "slimX/slim-master" > /dev/null
+  if [ $? -gt 0 ]; then
+    echo "ERROR: $REPO_PROJECT: does not have slimX/slim-master"
+    exit 1
+  fi
+  git checkout slim-master
+  if [ $? -gt 0 ]; then
+    echo "ERROR: $REPO_PROJECT: failed to checkout slimX/slim-master"
+    exit 1
+  fi
+  git pull slimX slim-master
+  if [ $? -gt 0 ]; then
+    echo "ERROR: $REPO_PROJECT: failed to pull slimX/slim-master"
+    exit 1
+  fi
+  git merge $slim_loc/kk4.4 -m "auto-merger: merge in from slim/kk4.4"
+  if [ $? -gt 0 ]; then
+    echo "ERROR: $REPO_PROJECT: failed to merge in slim/4.4"
+    exit 1
+  fi
+  echo "finished $REPO_PROJECT"
+'
+
+check "auto-merge slim failed, check log"
+
+fi
+fi
+
+if [[ $* != *--slim* ]]; then
+if [[ $* != *--push* ]]; then
+repo forall '-x' '/bin/bash' '-ec' '
+  if [ "$REPO_REMOTE" != "slimX" ]; then
+    exit 0
+  fi
+  cd $AUTO_TOP
+  blah=($(grep "$REPO_PATH" .auto-merge_ignore))
+  if [ -n "$blah" ]; then
+    bloo=${blah[1]}
+    if [ "$bloo" == "master" ] || [ "$bloo" == "" ]; then
+      echo "WARNING: ignoreing $REPO_PROJECT"
+      exit 0
+    fi
   fi
   cd $REPO_PATH
   echo "in $REPO_PATH"
   git branch -a | grep "aosp/master" > /dev/null
   if [ $? -gt 0 ]; then
     echo "ERROR: $REPO_PROJECT: does not have aosp/master"
-    return 1
+    exit 1
   fi
   git fetch aosp
   if [ $? -gt 0 ]; then
     echo "ERROR: $REPO_PROJECT: failed to fetch aosp/master"
-    return 1
+    exit 1
   fi
   git branch -a | grep "slimX/slim-master" > /dev/null
   if [ $? -gt 0 ]; then
     echo "ERROR: $REPO_PROJECT: does not have slimX/slim-master"
-    return 1
+    exit 1
   fi
   git checkout slim-master
   if [ $? -gt 0 ]; then
     echo "ERROR: $REPO_PROJECT: failed to checkout slimX/slim-master"
-    return 1
+    exit 1
   fi
   git pull slimX slim-master
   if [ $? -gt 0 ]; then
     echo "ERROR: $REPO_PROJECT: failed to pull slimX/slim-master"
-    return 1
+    exit 1
   fi
   git merge aosp/master -m "auto-merger: merge in from aosp/master"
   if [ $? -gt 0 ]; then
     echo "ERROR: $REPO_PROJECT: failed to merge in aosp/master"
-    return 1
-  fi
-  git push -u slimX slim-master
-  if [ $? -gt 0 ]; then
-    echo "ERROR: $REPO_PROJECT: failed to push to slimX/slim-master"
-    return 1
+    exit 1
   fi
   echo "finished $REPO_PROJECT"
 '
 
-check "auto-merge failed, check log"
+check "auto-merge master failed, check log"
+
+fi
+fi
+
+repo forall '-x' '/bin/bash' '-ec' '
+  if [ "$REPO_REMOTE" != "slimX" ]; then
+    exit 0
+  fi
+  cd $AUTO_TOP
+  blah=$(grep "$REPO_PATH" .auto-merge_ignore)
+  if [ -n "${blah}" ]; then
+    echo "WARNING: ignoreing $REPO_PROJECT"
+    exit 0
+  fi
+  cd $REPO_PATH
+  echo "in $REPO_PATH"
+  git push -u slimX slim-master
+  if [ $? -gt 0 ]; then
+    echo "ERROR: $REPO_PROJECT: failed to push to slimX/slim-master"
+    exit 1
+  fi
+  echo "pushed $REPO_PROJECT"
+'
+
+check "auto-merge push failed, check log"
 echo "all finished"
   
   
